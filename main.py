f619096458d4fcc44c2d96755d20de86382b2719
Edward Cromwell <edwardlc@umich.edu>
2025-03-14 06:53:03 -0400

    chore: installed modules

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
+# main.py: This file contains the 'main' function.Program execution begins and ends there.
+# .venv\Scripts\activate
 
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import manim as mn
+from manim import *
 
-def main():
-    print("This is the main function.")
+class DefaultTemplate(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set color and transparency
 
+        square = Square()  # create a square
+        square.flip(RIGHT)  # flip horizontally
+        square.rotate(-3 * TAU / 8)  # rotate a certain amount
 
-if __name__ == "__main__":
-    main()
+        self.play(Create(square))  # animate the creation of the square
+        self.play(Transform(square, circle))  # interpolate the square into the circle
+        self.play(FadeOut(square))  # fade out animation
+
