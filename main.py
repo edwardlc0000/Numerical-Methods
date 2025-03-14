699fc00a73edbbad5a9f3911ef9556cbda01f482
Edward Cromwell <edwardlc@umich.edu>
2025-03-13 23:57:35 -0400

    chore: init

---
+++ main.py
@@ -0,0 +1,13 @@
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import manim as mn
+
+def main():
+    print("This is the main function.")
+
+
+if __name__ == "__main__":
+    main()
