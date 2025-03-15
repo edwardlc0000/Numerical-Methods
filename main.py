738563613a00c17996d48c3b2ae07ab76b212b1a
Edward Cromwell <edwardlc@umich.edu>
2025-03-15 08:43:50 -0400

    feat: implemented basic vector plot

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 # main.py: This file contains the 'main' function.Program execution begins and ends there.
-# .venv\Scripts\activate
+# Created On: 2025-03-14
+# Created By: Edward Cromwell
+# An exploration of using numerical methods to solve differential equations in Python.
 
 import numpy as np
 from manim import *
 
-class DefaultTemplate(Scene):
+viridis_10: list[ManimColor] = [ManimColor("#440154"), ManimColor("#482878"), ManimColor("#3e4989"), ManimColor("#31688e"), ManimColor("#26828e"),
+           ManimColor("#1f9e89"), ManimColor("#35b779"), ManimColor("#6ece58"), ManimColor("#b5de2b"), ManimColor("#fde725")]
+
+class VectorMotion(Scene):
     def construct(self):
-        circle = Circle()  # create a circle
-        circle.set_fill(PINK, opacity=0.5)  # set color and transparency
+        func = lambda pos: np.sin(pos[1]) * RIGHT + np.cos(pos[0]) * UP
+        func = VectorField.scale_func(func, 0.5)
+        vector_field = ArrowVectorField(func, min_color_scheme_value=0, max_color_scheme_value=2, colors=viridis_10)
+        self.add(vector_field)
 
-        square = Square()  # create a square
-        square.flip(RIGHT)  # flip horizontally
-        square.rotate(-3 * TAU / 8)  # rotate a certain amount
+        dot = Dot()
+        vector_field.nudge(dot, -2, 60)
 
-        self.play(Create(square))  # animate the creation of the square
-        self.play(Transform(square, circle))  # interpolate the square into the circle
-        self.play(FadeOut(square))  # fade out animation
+        trace = TracedPath(dot.get_center, stroke_color=GRAY)
 
+        dot.add_updater(vector_field.get_nudge_updater())
+        self.add(trace, dot)
+        self.wait(6)
---
+++ manim tutorial.py
@@ -0,0 +1,115 @@
+# URL: https://docs.manim.community/en/stable/tutorials/quickstart.html
+# Description: Manim is an animation engine for explanatory math videos.
+
+# uv venv
+# uv pip install
+# .venv\Scripts\activate
+# draft: manim -pql main.py *
+# final: manim -pqh main.py *
+
+from manim import *
+import numpy as np
+
+class SquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set color and transparency
+
+        square = Square()  # create a square
+        square.flip(RIGHT)  # flip horizontally
+        square.rotate(-3 * TAU / 8)  # rotate a certain amount
+
+        self.play(Create(square))  # animate the creation of the square
+        self.play(Transform(square, circle))  # interpolate the square into the circle
+        self.play(FadeOut(square))  # fade out animation
+
+
+
+class ManimCELogo(Scene):
+    def construct(self):
+        self.camera.background_color = "#ece6e2"
+        logo_green = "#87c2a5"
+        logo_blue = "#525893"
+        logo_red = "#e07a5f"
+        logo_black = "#343434"
+        ds_m = MathTex(r"\mathbb{M}", fill_color=logo_black).scale(7)
+        ds_m.shift(2.25 * LEFT + 1.5 * UP)
+        circle = Circle(color=logo_green, fill_opacity=1).shift(LEFT)
+        square = Square(color=logo_blue, fill_opacity=1).shift(UP)
+        triangle = Triangle(color=logo_red, fill_opacity=1).shift(RIGHT)
+        logo = VGroup(triangle, square, circle, ds_m)  # order matters
+        logo.move_to(ORIGIN)
+        self.add(logo)
+
+
+
+class SquareAndCircle(Scene):
+    def construct(self):
+        circle = Circle()
+        circle.set_fill(PINK, opacity=0.5)
+
+        square = Square()
+        square.set_fill(BLUE, opacity=0.5)
+
+        square.next_to(circle, RIGHT, buff=0.5)
+        self.play(Create(circle), Create(square))
+
+
+
+class AnimatedSquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()
+        square = Square()
+
+        self.play(Create(square))
+        self.play(square.animate.rotate(PI / 4))
+        self.play(Transform(square, circle))
+        self.play(square.animate.set_fill(PINK, opacity=0.5))
+
+
+
+class DifferentRotations(Scene):
+    def construct(self):
+        left_square = Square(color=BLUE, fill_opacity=0.7).shift(2 * LEFT)
+        right_square = Square(color=GREEN, fill_opacity=0.7).shift(2 * RIGHT)
+        self.play(
+            left_square.animate.rotate(PI), Rotate(right_square, angle=PI), run_time=2
+        )
+        self.wait()
+
+
+
+class ScaleVectorFieldFunction(Scene):
+    def construct(self):
+        #pos[1] is the y-coordinate, pos[0] is the x-coordinate
+        func = lambda pos: np.sin(pos[1]) * RIGHT + np.cos(pos[0]) * UP
+        vector_field = ArrowVectorField(func)
+        self.add(vector_field)
+        self.wait()
+
+        func = VectorField.scale_func(func, 0.5)
+        self.play(vector_field.animate.become(ArrowVectorField(func)))
+        self.wait()
+
+
+
+class Nudging(Scene):
+    def construct(self):
+        func = lambda pos: np.sin(pos[1] / 2) * RIGHT + np.cos(pos[0] / 2) * UP
+        vector_field = ArrowVectorField(
+            func, x_range=[-7, 7, 1], y_range=[-4, 4, 1], length_func=lambda x: x / 2
+        )
+        self.add(vector_field)
+        circle = Circle(radius=2).shift(LEFT)
+        self.add(circle.copy().set_color(GRAY))
+        dot = Dot().move_to(circle)
+
+        vector_field.nudge(circle, -2, 60, True)
+        vector_field.nudge(dot, -2, 60)
+
+        circle.add_updater(vector_field.get_nudge_updater(pointwise=True))
+        dot.add_updater(vector_field.get_nudge_updater())
+        self.add(circle, dot)
+        self.wait(6)
+
+
